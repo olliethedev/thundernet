c3684f35eabd7cbab9aac26cc6f9e68b3d75f2f9
olliethedev <3martynov@gmail.com>
2023-07-19 16:44:00 -0400

    chore: assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import Logo from './assets/logo.png'
+import { NavBar } from './components/NavBar'
 
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 flex flex-col">
+      <NavBar />
     <div className="hero-content flex-col lg:flex-row">
       <Image src={Logo} className="max-w-sm rounded-lg shadow-2xl" alt="logo" />
       <div>
